b1d85a551c5e6934394c2d4cd57c79bd7ddba562
AnshulAnand <anandanshul001@gmail.com>
2022-11-14 14:10:32 +0530

    added binary-tree

---
+++ Binary-tree/1_depth_first_search.py
@@ -0,0 +1,16 @@
+
+from node_class import Node
+
+a = Node('a')
+b = Node('b')
+c = Node('c')
+d = Node('d')
+e = Node('e')
+f = Node('f')
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.left = f
+
---
+++ Binary-tree/node_class.py
@@ -0,0 +1,8 @@
+
+# binary tree Node class
+
+class Node:
+	def __init__(self, val):
+		self.val = val
+		self.left = None
+		self.right = None
