d99f56e9350d2a5e78c765cf909926c534511d51
AnshulAnand <anandanshul001@gmail.com>
2022-11-25 14:58:00 +0530

    added DFS algorithm

--- Binary-tree/1_depth_first_search.py
+++ Binary-tree/1_depth_first_search.py
@@ -1,4 +1,6 @@
 
+# print a array consisting all values(nodes) of a binary tree using DFS
+
 from node_class import Node
 
 a = Node('a')
@@ -14,3 +16,14 @@ b.left = d
 b.right = e
 c.left = f
 
+def depthFirstSearch(root):
+    if root == None: return []
+    res, stack = [], [root]
+    while len(stack) > 0:
+        current = stack.pop(-1)
+        res.append(current.val)
+        if current.right != None: stack.append(current.right)
+        if current.left != None: stack.append(current.left)
+    return res
+
+print(depthFirstSearch(a)) # prints ['a', 'b', 'd', 'e', 'c', 'f']
